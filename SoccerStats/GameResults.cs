e5d3a18c76dfe0e9ee9f717de292dad032b95b6a
Joe Gorsick <jgorsick@gmail.com>
2016-09-26 07:44:17 -0400

    Ready for the next module :)

--- SoccerStats/GameResults.cs
+++ SoccerStats/GameResults.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace SoccerStats
 {
-    class GameResults
+    public class GameResult
     {
         public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+        public int Goals { get; set; }
+        public int GoalAttempts { get; set; }
+        public int ShotsOnGoal { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PossesionPercent { get; set; }
+        public double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / (double)GoalAttempts;
+            }
+                
+         }
+
+    }
+    public enum HomeOrAway
+    {
+        Home,
+        Away
     }
 }
--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -24,16 +24,16 @@ namespace SoccerStats
             }
         }
 
-        public static List<string[]> ReadSoccerResults(string fileName)
+        public static List<GameResult> ReadSoccerResults(string fileName)
         {
-            var soccerResults = new List<string[]>();
+            var soccerResults = new List<GameResult>();
             using (var reader = new StreamReader(fileName))
             {
                 string line = "";
                 reader.ReadLine();
                 while ((line = reader.ReadLine()) !=null)
                 {
-                    var gameResult = new GameResults();
+                    var gameResult = new GameResult();
                     string[] values = line.Split(',');
 
                     DateTime gameDate;
@@ -41,12 +41,47 @@ namespace SoccerStats
                     {
                         gameResult.GameDate = gameDate;                    
                     }
-                    soccerResults.Add(values);
+                    gameResult.TeamName = values[1];
+
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+
+                   
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+
+                    
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                    double possesionPercent;
+                    if (double.TryParse(values[7], out possesionPercent))
+                    {
+                        gameResult.PossesionPercent = possesionPercent;
+                    }             
+
+
+
+                    soccerResults.Add(gameResult);
                 }
             }
             return soccerResults;
         }
     }
  }
-
-
